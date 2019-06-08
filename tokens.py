a8eaaac0428dbb4a17869bc5e4d70f80d341688e
Refael Yehuda <refaely@uoregon.edu>
2019-06-07 19:38:59 -0700

    added

--- tokens.py
+++
@@ -1,6 +0,0 @@
-
-#consumer key, consumer secret, access token, access secret.
-ckey="qiTTp6EnIHBPJQXGlaDq9liU0"
-csecret="YxE8cQK5FfMsTphI2bOe9k4KTm9s1yno4aY4C3EEitZ5hB2UTt"
-atoken="175862492-TJcTwPSuYAbU9odoi2xLoKwEUdQgZAUXnqVFMZ0Q"
-asecret="ODnDkvo2mntp8yEFMlQqXI7O3pjI3F1eUYzbdL72uJ4L8"
